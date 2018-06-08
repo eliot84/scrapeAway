c44d146b082efefd6bc85a2d985173caf3d594bb
Eliot Rodriguez <mr.ear84@gmail.com>
2018-06-08 14:58:04 -0400

    adding updates express setup

---
+++ server.js
@@ -0,0 +1,29 @@
+
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+var router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+	defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+
+app.use(router);
+
+app.listen(PORT, function(){
+	console.log("Listening on port: " + PORT);
+});
